111a081443e290ab6ec90af46f7586a34287ca8b
Martynas Kasparavicius <martis.kasparavicius@gmail.com>
2023-08-10 12:11:41 +0300

    Initial commit 1.0.0

---
+++ src/Helper.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace MoneyHelper;
+
+class Helper
+{
+    public static function formatMoney($unformated)
+    {
+        return bcdiv((string)$unformated, 1, 2);
+    }
+
+    /**
+     * Format cents to money with precision of 2 numbers
+     * @param mixed $unformated
+     * @return string
+     */
+    public static function formatCents($unformated)
+    {
+        return self::formatMoney(((int)$unformated) / 100);
+    }
+
+    public static function toCentsSafe(float $amount): int
+    {
+        return (int)((string)($amount * 100));
+    }
+
+    public static function calcVat($baseAmount, $vatPercent)
+    {
+        return $baseAmount * ($vatPercent / 100 / 100);
+    }
+
+    /**
+     * Format float value to cents safe
+     * @param $unformated
+     * @return int
+     */
+    public static function formatCentsToInt($unformated): int
+    {
+        if (is_int($unformated)) {
+            return (int)($unformated * 100);
+        }
+        if (!is_string($unformated)) {
+            $unformated = (string)$unformated;
+        }
+        $exploded = explode('.', $unformated, 2);
+        if (isset($exploded[1])) {
+            switch (strlen($exploded[1])) {
+                case 1:
+                    return (int)implode('', [...$exploded, '0']);
+                case 2:
+                    return (int)implode('', $exploded);
+                default:
+                    return (int)($exploded[0] . substr($exploded[1], 0, 2));
+            }
+        }
+        return ((int)$exploded[0]) * 100;
+    }
+}
